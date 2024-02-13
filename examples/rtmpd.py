409c951357b4ef8bc6701ca5a58e8721e3add56d
Seb <sebastien.pierrel@gmail.com>
2024-02-13 09:46:57 +0100

    cleanup

--- examples/rtmpd.py
+++ examples/rtmpd.py
@@ -11,7 +11,7 @@ height = 720
 process = (
     ffmpeg
     .input(url, listen=1)
-    .output('pipe:', format='rawvideo', pix_fmt='bgr24')
+    .output('pipe:', format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(width, height))
     .run_async(pipe_stdout=True)
 )
 
@@ -21,14 +21,10 @@ while True:
 
     frame = np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3])
 
-
     # Display the resulting frame
     cv2.imshow('frame', frame)
-    # import time
-    # filename = f"out.d/{time.strftime('%Y%m%d-%H%M%S')}.jpg"
-    # cv2.imwrite(filename, frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
-    # When everything done, release the capture
-    # rtmp.release()
+
 cv2.destroyAllWindows()
