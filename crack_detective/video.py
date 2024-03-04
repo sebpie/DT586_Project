68d3d05852989767f2c9a82162ac1cd5a5f1dde2
Seb <sebastien.pierrel@gmail.com>
2024-03-04 22:04:46 +0100

    add controls for input and output selection of streams

--- crack_detective/video.py
+++ crack_detective/video.py
@@ -1,5 +1,6 @@
+from .auth import login_required
 from . import VideoProcessing
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask import g
 
 def get_videoprocessor():
@@ -11,11 +12,61 @@ def get_videoprocessor():
 
 
 def init_app(app:Flask):
+
+    # TODO: initialise list of possible input video
+
     @app.route("/live_stream")
     def live_stream():
 
         videoprocessor = get_videoprocessor()
 
         return Response(videoprocessor.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    """
+    """
+    @app.route("/api/input", methods = ["GET", "PUT"])
+    @login_required()
+    def control_input():
+        match(request.method):
+            case "GET":
+                if 'video_input' not in g:
+                    return {"error" : "No video input selected"} , 400
+
+                return { 'video_input' : g.video_input }
+
+            case "PUT":
+                try:
+                    g.video_input = request.json["video_input"]
+                    # TODO: propagate the change
+
+                    return 200
+                except KeyError:
+                    return {"error" : "folder name is missing"}, 400
+
+        return {"error" : "Invalid request"}, 400
+
+    """
+    """
+    @app.route("/api/output", methods = ["GET", "PUT"])
+    @login_required()
+    def control_outut():
+        match(request.method):
+            case "GET":
+                if 'output_dir' not in g:
+                    return 404, "No folder selected"
+
+                return { 'output_dir' : g.output_dir }
+
+            case "PUT":
+                try:
+                    g.output_dir = request.json['output_dir']
+                    return 200
+                except KeyError:
+                    return {"error" : "folder name is missing"}, 400
+
+
+        return {"error" : "Invalid request"}, 400
+
+
 
     return
