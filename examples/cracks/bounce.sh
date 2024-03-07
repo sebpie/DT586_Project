bb0be934d399edab6ed18acce3d8f32d76562b14
Seb <sebastien.pierrel@gmail.com>
2024-03-07 15:24:24 +0100

    add sample data for mocking a stream

---
+++ examples/cracks/bounce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#ffmpeg -loop 1 -i testcard.png  -vf "crop=500:ih:'min((iw/10)*t,9*iw/10)':0" -t 5 -f matroska - | ffplay -
+
+ffmpeg -loop 1 -i testcard.png  -vf "crop=\
+	1280:720\
+	:'if(eq(mod(floor(st(8,t*100)/(iw - out_w)),2),0), \
+		mod(ld(8),iw - out_w), \
+		iw - out_w-mod(ld(8), iw - out_w))'\
+	:'\
+	if(eq(mod(floor(st(9,t*100)/(ih - out_h)),2),0), \
+		mod(ld(9),ih - out_h), \
+		ih-h-mod(ld(9), ih - out_h))
+	'"  -f matroska - | ffplay -
