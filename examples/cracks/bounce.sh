90dcb80c46a974fdaf14855080d3c3839f238199
Seb <sebastien.pierrel@gmail.com>
2024-03-07 19:16:43 +0100

    input file

--- examples/cracks/bounce.sh
+++ examples/cracks/bounce.sh
@@ -2,8 +2,15 @@
 
 #ffmpeg -loop 1 -i testcard.png  -vf "crop=500:ih:'min((iw/10)*t,9*iw/10)':0" -t 5 -f matroska - | ffplay -
 
-ffmpeg -loop 1 -i testcard.png  -vf "crop=\
-	1280:720\
+echo "input file: ${1}"
+
+FILE=$1
+[ -z "${FILE}"] && FILE="testcard.png"
+
+echo "ffmpeg to use ${FILE}"
+
+ffmpeg -loop 1 -i $FILE  -vf "crop=\
+	640:480\
 	:'if(eq(mod(floor(st(8,t*100)/(iw - out_w)),2),0), \
 		mod(ld(8),iw - out_w), \
 		iw - out_w-mod(ld(8), iw - out_w))'\
