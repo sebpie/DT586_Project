35cfad986b254b10814969b8a0b6677417b24664
parvathi15 <55029879+parvathi15@users.noreply.github.com>
2024-03-16 05:11:41 +0100

    implement file system view for gallery
    implement file system view for gallery

--- crack_detective/static/script.js
+++ crack_detective/static/script.js
@@ -112,20 +112,20 @@ document.addEventListener('DOMContentLoaded', function() {
         updateFolderList();
     });
 
-// Event listener for closing the folder creation popup
+
 document.getElementById('close-popup').addEventListener('click', function() {
     const folderNameInput = document.getElementById('folder-name');
     const responseMessage = document.getElementById('response-message');
 
-    folderNameInput.value = ''; // Clearing the input field
-    responseMessage.innerText = ''; // Clearing the response message
+    folderNameInput.value = ''; 
+    responseMessage.innerText = ''; 
 
 
 
     document.getElementById('create-folder-popup').style.display = 'none';
     updateFolderList();
 });
-    // Event listener for folder action dropdown
+
 // Event listener for folder action dropdown
 document.getElementById('folder-action').addEventListener('change', function() {
     var selectedOption = this.value;
@@ -134,7 +134,6 @@ document.getElementById('folder-action').addEventListener('change', function() {
         console.log("open create folder");
     } else if (selectedOption === 'list') {
         console.log("list folder");
-        // Show folder list
         document.getElementById('folder-list').style.display = 'block';
     } else {
         // Clear the create folder popup and send the captured image to the selected folder
@@ -183,7 +182,7 @@ document.getElementById('folder-action').addEventListener('change', function() {
             .then(response => response.json())
             .then(data => {
                 console.log(data.message);
-                updateFolderList(); // Refresh folder list after deletion
+                updateFolderList(); 
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -236,7 +235,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// Function to display a notification message
+// display a notification message
 function showNotification(message) {
     // Create a notification element
     const notification = document.createElement('div');
@@ -265,101 +264,184 @@ function showNotification(message) {
   //settings page
 
   document.addEventListener('DOMContentLoaded', function() {
-    // Retrieve the username passed from the Flask route
     var username = "{{ username }}";
-    // Set the value of the username input field
     document.getElementById('username').value = username;
 });
 
 //gallery page
 
+<<<<<<< Updated upstream
 const images = ["../static/images/image1.jpg", "../static/images/image2.jpg",
 "../static/images/image3.jpg","../static/images/image4.jpg","../static/images/image5.jpg",
 "../static/images/image6.jpg"]; // Add more images as needed
+=======
+>>>>>>> Stashed changes
 let currentIndex = 0;
+let images = [];
 
 document.addEventListener('DOMContentLoaded', function() {
-  const galleryImages = document.querySelectorAll('.gallery-image');
-
-  galleryImages.forEach(function(image, index) {
-    image.addEventListener('click', function() {
-      currentIndex = index;
-      openOverlay(images[currentIndex]);
-    });
-  });
-
-  // Listen for keyboard events
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowLeft') {
-      prevImage();
-    } else if (event.key === 'ArrowRight') {
-      nextImage();
-    }
-  });
-});
-
-function openOverlay(src) {
-  const overlay = document.getElementById('overlay');
-  const mainImageOverlay = document.getElementById('mainImageOverlay');
-
-  mainImageOverlay.src = src;
-  overlay.style.display = 'block';
-}
-
-function closeOverlay() {
-  const overlay = document.getElementById('overlay');
-  overlay.style.display = 'none';
-}
-
-function prevImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  document.getElementById('mainImageOverlay').src = images[currentIndex];
-}
-
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  document.getElementById('mainImageOverlay').src = images[currentIndex];
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to add event listeners to folders in the gallery
     function addFolderEventListeners() {
         document.querySelectorAll('.folder-icon').forEach(folder => {
             folder.addEventListener('click', function() {
                 const folderName = this.querySelector('h6').textContent;
-                console.log(folderName)
                 loadImages(folderName);
             });
         });
     }
 
-  // Function to load images for a specific folder
-function loadImages(folderName) {
-    console.log(folderName)
-    fetch(`/api/folders/${folderName}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const imagesContainer = document.querySelector('.images-container');
-            imagesContainer.innerHTML = ''; // Clear the previous images
+    function loadImages(folderName) {
+        fetch(`/api/folders/${folderName}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Images Data:', data); 
+                
+                const imagesContainer = document.querySelector('.images-container');
+                imagesContainer.innerHTML = '';
+                images = []; 
 
-            if (data.images && data.images.length > 0) {
-                data.images.forEach(imageUrl => {
-                    const img = document.createElement('img');
-                    img.src = imageUrl;
-                    img.alt = 'Image';
-                    img.classList.add('gallery-image');
-                    imagesContainer.appendChild(img);
+                document.querySelectorAll('.folder-icon').forEach(folder => {
+                    folder.style.display = 'none';
                 });
-            } else {
-                imagesContainer.innerHTML = '<p>No images found in this folder</p>';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
 
+                if (data.images && data.images.length > 0) {
+                    data.images.forEach(filename => {
+                        const filenameParts = filename.split('/');
+                        const correctedFilename = filenameParts[filenameParts.length - 1];
+                        
+                        const img = document.createElement('img');
+                        img.src = `/api/folders/${folderName}/${correctedFilename}`; 
+                        img.alt = 'Image';
+                        img.classList.add('gallery-image');
+                        imagesContainer.appendChild(img);
+                        images.push(img.src );
+                        
+                        
+                        img.addEventListener('click', function() {
+                            console.log("clicked image")
+                            const imageUrl = this.src; 
+                            console.log(imageUrl)
+                            openOverlay(imageUrl); 
+                        });
+                    });
+                } else {
+                    imagesContainer.innerHTML = '<p>No images found in this folder</p>';
+                }
+
+                // back button
+                const backButton = document.getElementById('back-button');
+                backButton.style.display = 'inline-block';
+                
+                
+                backButton.addEventListener('click', function() {
+                   
+                    document.querySelectorAll('.folder-icon').forEach(folder => {
+                        folder.style.display = 'block';
+                    });
+                  
+                    backButton.style.display = 'none';
+                  
+                    imagesContainer.innerHTML = '';
+                    images = [];
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error); 
+            });
+    }
+    
     // Call the function to add event listeners to folders
     addFolderEventListeners();
 });
+
+
+
+function openOverlay(imageUrl) {
+    console.log(imageUrl)
+    const overlay = document.getElementById('overlay');
+    const fullSizeImage = document.getElementById('mainImageOverlay');
+
+    fullSizeImage.src = imageUrl;
+    overlay.style.display = 'block'; 
+}
+
+
+function closeOverlay() {
+const overlay = document.getElementById('overlay');
+overlay.style.display = 'none';
+}
+
+// keyboard events
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        prevImage();
+    } else if (event.key === 'ArrowRight') {
+        nextImage();
+    }
+});
+function prevImage() {
+    console.log("prev clicked")
+currentIndex = (currentIndex - 1 + images.length) % images.length;
+console.log(currentIndex)
+document.getElementById('mainImageOverlay').src = images[currentIndex];
+}
+
+function nextImage() {
+    console.log("next clicked")
+currentIndex = (currentIndex + 1 + images.length) % images.length;
+console.log(currentIndex)
+document.getElementById('mainImageOverlay').src = images[currentIndex];
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fullScreenBtn = document.getElementById('full-screen-btn');
+    const uploadContainer = document.getElementById('upload-container');
+    let isFullScreen = false;
+
+    // Function to toggle full screen mode
+    function toggleFullScreen() {
+        if (!isFullScreen) {
+            if (uploadContainer.requestFullscreen) {
+                uploadContainer.requestFullscreen();
+            } else if (uploadContainer.webkitRequestFullscreen) { 
+                uploadContainer.webkitRequestFullscreen();
+            } else if (uploadContainer.msRequestFullscreen) { 
+                uploadContainer.msRequestFullscreen();
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) { 
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { 
+                document.msExitFullscreen();
+            }
+        }
+
+        isFullScreen = !isFullScreen;
+    }
+
+    // for full screen button
+    fullScreenBtn.addEventListener('click', toggleFullScreen);
+
+    //  for exiting full screen mode 
+    document.addEventListener('fullscreenchange', function(event) {
+        if (!document.fullscreenElement) {
+            isFullScreen = false;
+        }
+    });
+
+    document.addEventListener('webkitfullscreenchange', function(event) {
+        if (!document.webkitFullscreenElement) {
+            isFullScreen = false;
+        }
+    });
+
+    document.addEventListener('msfullscreenchange', function(event) {
+        if (!document.msFullscreenElement) {
+            isFullScreen = false;
+        }
+    });
+});
+
+ 
