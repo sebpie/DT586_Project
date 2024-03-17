5e4843aa2bf5b2da90908e34d9d5bde0dfb557d7
seb <sebastien.pierrel@gmail.com>
2024-03-17 15:00:37 +0100

    Standardise the naming of the models (.keras files)

--- train_default_models.bat
+++ train_default_models.bat
@@ -1,5 +1,5 @@
 python -m crack_detective.cnncmd --cnn Orig --epochs=5  --size 80  --save CNN_Orig-80x80-Mendelay_FULL.keras
 python -m crack_detective.cnncmd --cnn Orig --epochs=5  --size 256 --save CNN_Orig-256x256-Mendelay_FULL.keras
-python -m crack_detective.cnncmd --cnn Orig --epochs=20 --size 80  --save Win_CNN_Orig-80x80-Mendelay_FULL-20epochs.keras
-python -m crack_detective.cnncmd --cnn Orig --epochs=20 --size 256 --save Win_CNN_Orig-256x256-Mendelay_FULL-20epochs.keras
+python -m crack_detective.cnncmd --cnn Orig --epochs=20 --size 80  --save CNN_Orig-80x80-Mendelay_FULL-20epochs.keras
+python -m crack_detective.cnncmd --cnn Orig --epochs=20 --size 256 --save CNN_Orig-256x256-Mendelay_FULL-20epochs.keras
 python -m crack_detective.cnncmd --cnn Orig --epochs=20 --size 224 --save CNN_Orig-224x224-Mendelay_FULL.keras
