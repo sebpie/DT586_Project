d42d2602d3d3cdd80c5e1b24c1c7441d54a39662
seb <sebastien.pierrel@gmail.com>
2024-03-20 15:28:23 +0100

    Remove defunct code (MockedModel and single patch predict)

--- crack_detective/crack_detector.py
+++ crack_detective/crack_detector.py
@@ -20,18 +20,6 @@ color_scale = [
 ]
 
 
-# class MockedModel(object):
-#     width = 224
-#     height = 224
-#     channels = 3
-#     def predict(self, image, batch=False):
-#         if batch:
-#             # Should reduce 3 dimensions from input shape
-#             # print(f"input array shape: {image.shape}")
-#             return np.random.rand( 3, 6, 1)
-#         else:
-#             return random.random()
-
 def getColor(predict):
     for threshold, color in color_scale:
         if(predict >= threshold):
@@ -227,38 +215,3 @@ class CrackDetector(utils.Subscribable):
             """Step 5: Publish the results to subscribers"""
             # print(f"Done with this frame.")
             self.publish(processed_frame)
-
-    # def _worker(self):
-    #     for frame in self.buffer_in.stream():
-    #         # print(f"Process frame size {frame.shape}")
-    #         """Step 1: Patchify the frame in patches"""
-    #         patches = patchify.patchify(frame.copy(), (self.model.width, self.model.height, self.model.channels), step=224 )
-
-    #         """Step 2: Predict each patch with cracks"""
-    #         print(f"Process {(len(patches), patches.shape)} patches for this frame")
-    #         for row in patches:
-    #             for col in row:
-    #                  for patch in col:
-    #                     # print(f"patch size: {patch.shape}")
-    #                     # print(patch)
-
-    #                     results = self.model.predict(col)
-    #                     print(f"predict results shape: {results.shape}")
-
-    #                     if results[0][0] > 0.5:
-    #                         color = (0, 255, 0) # GREEN
-    #                     else:
-    #                         color = (0, 0, 255) # RED
-    #                     patch.setflags(write=1)
-
-    #                     """Step 3: Apply visualisation to positive patches"""
-    #                     cv2.rectangle(patch, (1, 1), (self.model.width -2, self.model.height -2), color=color, thickness=2 )
-    #                     # print("done drawing")
-
-
-    #         """Step 4: Stitch up! (unpatchify)"""
-    #         processed_frame = patchify.unpatchify(patches, frame.shape)
-
-    #         """Step 5: Publish the results to subscribers"""
-    #         # print(f"Done with this frame.")
-    #         self.publish(processed_frame)
